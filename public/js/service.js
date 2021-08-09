2d5050c4603f333315a4bee666448f25342630fd
dayoe <70726916+dayoe@users.noreply.github.com>
2021-08-09 11:09:33 -0700

    fixed 'container not found' error

--- public/js/service.js
+++ public/js/service.js
@@ -1,24 +1,37 @@
+/*module.exports = function (){
+    const express = require('express');
+    const router = express.Router();
+
+
+}
+*/
+
+
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(drawChart).then(response => {
+    console.log(response);
+}).catch(e => {
+    console.log(e);
+});
 
 function drawChart() {
-var data = new google.visualization.DataTable();
-data.addColumn('string', 'Topping');
-data.addColumn('number', 'Slices');
-data.addRows([
-['Mushrooms', 3],
-['Onions', 1],
-['Olives', 1],
-['Zucchini', 1],
-['Pepperoni', 2]
-]);
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Topping');
+    data.addColumn('number', 'Slices');
+    data.addRows([
+        ['Mushrooms', 3],
+        ['Onions', 1],
+        ['Olives', 1],
+        ['Zucchini', 1],
+        ['Pepperoni', 2]
+    ]);
 
-// Set chart options
-var options = {'title':'How Much Pizza I Ate Last Night',
-'width':400,
-'height':300};
+    // Set chart options
+    var options = {'title':'How Much Pizza I Ate Last Night',
+                    'width':400,
+                    'height':300};
 
-// Instantiate and draw our chart, passing in some options.
-var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-chart.draw(data, options);
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+    chart.draw(data, options);
 }
